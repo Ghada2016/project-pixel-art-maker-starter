a0a6f50f8bb83c3ecca82eff9fbfedb6729ac5f4
Ghada2016 <gh_mostafa@hotmail.com>
2018-01-02 13:52:06 +0200

    Add files via upload

--- designs.js
+++ designs.js
@@ -3,8 +3,44 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
-// Your code goes here!
-
+function makeGrid(height, width) {
+    'use strict';
+    let grid;
+    
+    for(let i = 0 ; i< height;i++){
+        grid+=('<tr>')
+        
+        for(let j = 0; j< width;j++){
+            grid+=('<td></td>')
+        }
+        grid+=('</tr>');
+    }
+    $('#pixel_canvas').html(grid);
 }
+
+/* submit button*/
+
+$(document).ready(function(){
+    $('#sub-btn').click(function(evt){
+        let height  = $('#input_height').val();
+        let width   = $('#input_width').val();
+        evt.preventDefault();
+        makeGrid(height,width);
+        
+    });
+    
+});
+
+/*Click a cell in the grid to fill that cell with the chosen color*/
+
+let color, cell;
+
+color = $('#colorPicker');
+cell  = $('#pixel_canvas');
+cell.click(function(evt){
+    color = $('#colorPicker').val();
+    $(evt.target).css("background-color", color);
+});
+    
+
+
